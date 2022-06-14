d8cf29e0ddf8cf159e70ff4941c2d670a41dbab7
Ender Invader <derek.scandrett@cox.net>
2022-06-14 00:18:32 -0700

    Quick Changelog change

--- src/shared/changelog.tsx
+++ src/shared/changelog.tsx
@@ -6,10 +6,10 @@ export function getChangelog() {
 						<hr></hr>
 						<p>v2.0 (Nettogrof)</p><p>New update: "TestSnake" section is now working,<br></br>be sure to enter the full url with the /move at the end</p>
 						<hr></hr>
-						<p>v3.0 (EnderInvader)</p><p>New update: Hazards! Rulesets! Maps!, along side an updated framework and<br></br>Battlesnake json format</p>
+						<p>v3.0 (EnderInvader)</p><p>New update: Hazards! Rulesets! Maps!, along side<br></br>an updated framework and Battlesnake json format</p>
 						
 						<br></br>
 						<hr></hr>
-						<p>Upcoming</p><p>Customizations, Fix Test Snake</p>
+						<p>Upcoming</p><p>Customizations, Better Snake Control, Fix Test Snake</p>
 					</TitledContainer>
 }
