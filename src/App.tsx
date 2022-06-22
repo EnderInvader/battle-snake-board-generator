c0adfc7de2fb3df86038ef0e000db2972e06d3d7
EnderInvader <enderinvader.end@gmail.com>
2022-06-21 18:18:23 -0700

    Removing unneeded things

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-/*import logo from './logo.svg';*/
 import './App.css';
 import { IBoardState } from './types/IBoardState.interface';
 import { Board } from './components/board';
